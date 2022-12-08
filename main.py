f3076af13cbf4d4bf62f5e6ce153aff34e13adc5
Sonam Chand <sonam.chand@kasna.com.au>
2022-12-08 22:15:10 +0000

    changes

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def move():
             move= 'L'
         else: 
             move= 'F'
-        yield move
+        return move
    
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
